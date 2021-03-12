ffa4406cad1edd8118b51e0e1d514520baeb504c
Natalya Kopylova <civillale@yandex.ru>
2021-03-12 16:47:45 +0300

    lesson3

--- lesson3/main.swift
+++ lesson3/main.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  main.swift
 //  lesson3
 //
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
-
+ let users = [("Иванов", "Иван", "Иванович"),
+              ("Петров", "Петр", "Петрович"),
+              ("Сидоров", "Сидор", "Сидорович"),
+              ("Александров", "Александр", "Александрович")]
+ 
+ enum PrintUserInfoMode: String {
+    case short = "Сокр"
+    case middle = "Обычный"
+    case full = "Подробный"
+ }
+ 
+ func printUsers(_users: [(String, String, String)], mode: PrintUserInfoMode) {
+    print(mode.rawValue)
+    for user in users {
+        switch mode {
+        case .full:
+            print(user.0, user.1, user.2)
+        case .middle:
+            print(user.0, user.1)
+        case .short:
+            print(user.0)
+        }
+    }
+ }
+ printUsers(_users: users, mode: .full)
